8ab6ba2cb5e05a233e7bc56c2f4775cfe0361409
Wassim El Mourabit <wassimelmorabit7@gmail.com>
2025-02-04 20:42:29 +0100

    update the .gitignore file

--- app/core/Model.php
+++ app/core/Model.php
@@ -3,7 +3,7 @@ namespace App\Core;
 use Illuminate\Database\Eloquent\Model as EloquentModel;
 
 class Model extends EloquentModel {
-    // Configuration d'Eloquent
+    
     public static function init() {
         $capsule = new \Illuminate\Database\Capsule\Manager;
         $capsule->addConnection(include __DIR__ . '/../../config/config.php');
